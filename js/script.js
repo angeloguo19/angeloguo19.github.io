8e5dfc7a33d342fec679b57d74e3f1566342a43a
Angelo Guo <angelo.guo@duke.edu>
2019-10-01 18:48:40 -0400

    Working navbar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$('.dropdown').on('show.bs.dropdown', function(e){
-  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
-});
-
-$('.dropdown').on('hide.bs.dropdown', function(e){
-  $(this).find('.dropdown-content').first().stop(true, true).slideUp(200);
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top;
+    });
 });
